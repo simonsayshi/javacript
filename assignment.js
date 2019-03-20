f7c6b1aad646112ca0430c70b965f2ae235dd021
simonsayshi <31574218+simonsayshi@users.noreply.github.com>
2019-03-19 23:34:35 -0400

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,27 @@
+﻿function queue()
+{
+    this.elements = [];
+}
+
+
+queue.prototype.push = function (e) {
+    this.elements.push(e);
+}
+
+queue.prototype.pop = fcuntion()
+{
+    return this.elements.shift();
+}
+
+queue.prototype.isEmpty = function () {
+    return this.elements.length == 0;
+}
+
+queue.prototype.front = function () {
+    return !this.isEmpty() ? this.elements[0] : undefined;
+}
+
+queue.prototype.back = function () {
+    return !this.isEmpty() ? this.elements[-1] : undefined;
+}
+
